b8b21f673c386a7c38947d58fb79b6414dddd5b3
jcook <jcook@pykl.com>
2013-04-25 13:22:34 -0400

    Added JResig's subClass function and Object.extend().

--- src/main/webapp/WEB-INF/lib/migrate/lib/main.js
+++ src/main/webapp/WEB-INF/lib/migrate/lib/main.js
@@ -2,3 +2,88 @@ exports.Google = {
     Mail: require( './google/mail/google-mail' ).Service
 };
 
+
+if (typeof Object.extend !== 'function') {
+    Object.extend = function extend( orig ) {
+        var args = Array.prototype.slice.call( arguments, 0 );
+        var result = args.shift();
+        var arg = args.shift();
+        while ( arg ) {
+            for ( var prop in arg ) {
+                if ( arg.hasOwnProperty( prop ) ) {
+                    result[prop] = arg[prop];
+                }
+            }
+            arg = args.shift();
+        }
+        return result;
+    };
+}
+
+/**
+ * Extends the Object class with John Resig's class inheritance recipe.
+ */
+(function () {
+    if (typeof Object.subClass === 'function') return;
+
+    var initializing = false,
+        superPattern = // Determine if functions can be serialized
+            /xyz/.test( function () {
+                xyz;
+            } ) ? /\b_super\b/ : /.*/;       //#1
+
+    // Creates a new Class that inherits from this class
+    Object.subClass = function ( properties ) {                           //#2
+        var _super = this.prototype;
+
+        // Instantiate a base class (but only create the instance,
+        // don't run the init constructor)
+        initializing = true;                                              //#3
+        var proto = new this();                                           //#3
+        initializing = false;                                             //#3
+
+        // Copy the properties over onto the new prototype
+        for ( var name in properties ) {                                    //#4
+            // Check if we're overwriting an existing function
+            proto[name] = typeof properties[name] == "function" &&
+                typeof _super[name] == "function" &&
+                superPattern.test( properties[name] ) ?
+                (function ( name, fn ) {                                        //#5
+                    return function () {
+                        var tmp = this._super;
+
+                        // Add a new ._super() method that is the same method
+                        // but on the super-class
+                        this._super = _super[name];
+
+                        // The method only need to be bound temporarily, so we
+                        // remove it when we're done executing
+                        var ret = fn.apply( this, arguments );
+                        this._super = tmp;
+
+                        return ret;
+                    };
+                })( name, properties[name] ) :
+                properties[name];
+        }
+
+        // The dummy class constructor
+        function Class() {                                                   //#6
+            // All construction is actually done in the init method
+            if ( !initializing && this.init )
+                this.init.apply( this, arguments );
+        }
+
+        // Populate our constructed prototype object
+        Class.prototype = proto;                                             //#7
+
+        // Enforce the constructor to be what we expect
+        Class.constructor = Class;                                           //#8
+
+        // And make this class extendable
+        Class.subClass = arguments.callee;                                   //#9
+
+        return Class;
+    };
+})();
+
