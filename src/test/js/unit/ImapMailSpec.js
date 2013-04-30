5f4cd844b0549343563ceddefd772f064ac4c6e9
Anthony <aclever@pykl.com>
2013-04-29 20:50:01 -0400

    Attachment test first pass.

--- src/test/js/unit/ImapMailSpec.js
+++ src/test/js/unit/ImapMailSpec.js
@@ -74,6 +74,88 @@ function writeTests(service) {
 
             // All done.
         });
+
+        // Todo: refactor this DRY-style.
+        it('should allow me to write an email with an attachment to the server', function () {
+            // We need a mock email, and this email account isn't wiped, so let's generate a semi-random string and test for it.
+            // Thanks, stack overflow: http://stackoverflow.com/questions/6248666/how-to-generate-short-uid-like-ax4j9z-in-js
+            var uid = ("0000" + (Math.random() * Math.pow(36, 4) << 0).toString(36)).substr(-4);
+
+            // Now we need to start writing a new email.
+            // The message constructor takes a session as an argument.
+            var email = new javax.mail.internet.MimeMessage(service.session);
+
+            // Now lets fill out some dummy data in the message.
+            email.setFrom(new javax.mail.internet.InternetAddress('test@migrate.io'));
+            email.setRecipient(javax.mail.Message.RecipientType.TO, new javax.mail.internet.InternetAddress(service.email));
+
+            // This is the main thing we'll test for, since its the one that will be unique-ish.
+            email.setSubject(uid);
+
+            var sentDate = new java.util.Date();
+
+            // This one is also going to be tested, since it **will** be unique.
+            email.setSentDate(sentDate);
+
+            var bodyPart = new javax.mail.internet.MimeBodyPart();
+
+            bodyPart.setText('Test email w/ attachment.');
+
+            var multipart = new javax.mail.internet.MimeMultipart();
+            multipart.addBodyPart(bodyPart);
+
+            var attachment = new javax.mail.internet.MimeBodyPart();
+
+            var json = JSON.stringify({
+                'test': uid
+            });
+
+            //Found an easier way to do the attachment. using it
+            var ds = new javax.mail.util.ByteArrayDataSource(json, 'application/json');
+            attachment.setDataHandler(new javax.activation.DataHandler(ds));
+            multipart.addBodyPart(attachment);
+
+            email.setContent(multipart);
+            // Alright, our email is constructed, lets append it to the server.
+
+            // We have to initialize an array, since that's what write takes.
+            var emails = [email];
+
+            // And with this called, we should be ready to test.
+            var result = service.write('INBOX', emails);
+
+            // Our returned successCount should be 1, and our errors should be empty
+            expect(result.successCount).toBe(1);
+            expect(result.errors).toEqual([]);
+
+            // Now we'll need to get the email we just wrote. Probably gonna break out of the regular api here and call some **java** directly.
+
+            // Hm, theoretically it should be the last written email, so lets grab that one.
+            var folder = service.store.getFolder('INBOX');
+            var count = folder.getMessageCount();
+
+            // This shouldn't be necessary, as it should still be open from the write call. However, we include this just to be sure.
+            if (!folder.isOpen()) {
+                folder.open(javax.mail.Folder.READ_ONLY);
+            }
+
+            // Since it should be the last email, we call getMessages with a new array containing the count.
+            // Message numbers shouldn't have gaps, so this should return the last email.
+            var testEmail = folder.getMessages([count]);
+
+            // getMessages returns an array, so lets alias that to a single message object.
+            testEmail = testEmail[0];
+
+            // Alright, we have our email, lets test it.
+            expect(testEmail.getSubject()).toBe(uid);
+            expect(testEmail.getSentDate().toString()).toEqual(sentDate.toString());
+            expect(testEmail.getSender().toString()).toBe('test@migrate.io');
+
+            // This one is probably uh, not right.
+            expect(testEmail.getRecipients(javax.mail.Message.RecipientType.TO)[0].toString()).toBe(service.email);
+
+            // All done.
+        });
     });
 }
 
