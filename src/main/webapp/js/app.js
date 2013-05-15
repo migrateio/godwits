1d452e5d16df89d297082e31b18082b88e1904f4
jcook <jcook@pykl.com>
2013-05-15 13:51:33 -0400

    Added the ng-view and made the home page a partial.

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -4,15 +4,19 @@
 angular.module('migrate', []).
     config(['$routeProvider', function($routeProvider) {
 
-    $routeProvider.when('/view1', {
-        templateUrl: 'partials/partial1.html',
-        controller: 'MyCtrl1'
+    $routeProvider.when('/', {
+        templateUrl: 'partials/home.html'
     });
-    $routeProvider.when('/view2', {
-        templateUrl: 'partials/partial2.html',
-        controller: 'MyCtrl2'
+    $routeProvider.when('/signin', {
+        templateUrl: 'partials/signin.html'
+    });
+    $routeProvider.when('/migrations', {
+        templateUrl: 'partials/signin.html'
+    });
+    $routeProvider.when('/about', {
+        templateUrl: 'partials/signin.html'
     });
     $routeProvider.otherwise({
-        redirectTo: '/view1'
+        redirectTo: '/'
     });
 }]);
