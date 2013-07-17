1f42f832a157d8fc90292a1478a62b3b89004dd2
Anthony <aclever@pykl.com>
2013-07-17 15:39:28 -0400

    Documentation for google drive stuff, as well as making it a little more robust. #1

--- src/main/webapp/WEB-INF/lib/migrate/lib/google/drive/google-drive.js
+++ src/main/webapp/WEB-INF/lib/migrate/lib/google/drive/google-drive.js
@@ -15,7 +15,7 @@ var httpClient = require( 'ringo/httpclient' );
  */
 exports.Drive = function ( credentials ) {
 
-    var files = [];
+    this.credentials = credentials;
 
     function buildURI( shorthand, id ) {
         const API_KEY = 'AIzaSyCoimO0Pl6XrijUuqKiTyBRR4C5WrvALaE';
@@ -26,24 +26,35 @@ exports.Drive = function ( credentials ) {
         return baseurl + id ? id + '?key=' + API_KEY : '?key=' + API_KEY + '&maxresults=' + MAX_RESULTS;
     }
 
+    /**
+     * Gotta be a better way to do this.
+     * @param shorthand
+     * @returns {string}
+     */
     function getAppropriateURI( shorthand ) {
         switch ( shorthand ) {
             case 'listFiles':
                 return 'https://www.googleapis.com/drive/v2/files';
             case 'getFile':
                 return 'https://www.googleapis.com/drive/v2/files/';
+            case 'upload':
+                return 'https://www.googleapis.com/upload/drive/v2/files';
             default:
                 throw {
                     status : 500,
-                    message : 'Expected listFiles or getFile, got neither'
+                    message : 'Expected listFiles, upload, or getFile, got neither'
                 };
         }
     }
 
-    function init() {
 
-    }
-
+    /**
+     *
+     * basic read. returns an array of file resources:
+     * https://developers.google.com/drive/v2/reference/files
+     *
+     * @returns {*}
+     */
     function read() {
 
         var url = buildURI( 'listFiles' );
@@ -51,28 +62,65 @@ exports.Drive = function ( credentials ) {
         var opts = {
             url : url,
             headers : {
-                Authorization : 'Bearer ' + credentials.accesskey
+                Authorization : 'Bearer ' + this.credentials.accesskey
             }
         };
 
         var exchange = httpClient.request( opts );
 
         if ( exchange.status === 200 ) {
-            //hooray shit works
-            var json = exchange.content;
-            files.push()
+            return exchange.content;
         }
 
         if ( exchange.status === 401 ) {
             //we need to refresh the token.
-            refreshToken( credentials )
+            this.credentials = refreshToken( this.credentials );
+            java.lang.Thread.sleep( 2000 );
+            return read();
         }
     }
 
-    function write( files ) {
 
+    /**
+     *
+     * Takes an array of files resources to upload to google drive. File resources have download/export links.
+     * This function loops through a specific job chunked and passes through the specified files before returning a result.
+     * @param files
+     * @returns {Array}
+     */
+    function write( files ) {
+        var result = [];
+
+        for ( var i = 0; i < files.length; i++ ) {
+            result.push( passthroughToDrive( files[i] ) );
+        }
+
+        return result;
     }
 
+    /**
+     * Here's where the bulk of the work gets done.
+     *
+     * In here we take a singular file resource, download it, remove any metadata that we
+     * aren't allowed to upload (download link would be one).
+     *
+     * Then we download the file and upload to drive with the metadata.
+     * @param file
+     */
+    function passthroughToDrive( file ) {
+        var metadata = JSON.stringify({
+            description: file.description,
+            indexableText: file.indexableText,
+            labels: file.labels,
+            lastViewedByMeDate: file.lastViewedByMeDate,
+            mimeType: file.mimeType,
+            modifiedDate: file.modifiedDate,
+            parents: file.parents,
+            title: file.title
+        });
+
+
+    }
 
     function refreshToken( credentials ) {
         var opts2 = {
@@ -92,6 +140,17 @@ exports.Drive = function ( credentials ) {
         };
         log.info( 'Making request: {}', JSON.stringify( opts2, null, 4 ) );
         var exchange = httpClient.request( opts2 );
+
+        if ( exchange.status === 200 ) return exchange.content;
+
+        throw 'Error refreshing credentials';
+    }
+
+    function updateCredentials( rawcreds ) {
+        return {
+            access_key : rawcreds.access_key,
+            refresh_token : rawcreds.refresh_token
+        };
     }
 
     return {};
