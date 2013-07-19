086915ba3750313392dbb8525f6b3259468fda18
Anthony <aclever@pykl.com>
2013-07-19 17:13:37 -0400

    Poke around a bit. Nothing fancy.

---
+++ src/test/js/google/calendarSpec.js
@@ -0,0 +1,11 @@
+describe('The google calendar service', function () {
+
+    it('should allow me to read all events/calendars', function () {
+
+    });
+
+    it('should write events/calendars', function () {
+
+    });
+
+});
---
+++ src/test/js/google/contactsSpec.js
@@ -0,0 +1,11 @@
+describe('The google contacts service', function () {
+
+    it('should allow me to read all contacts', function () {
+
+    });
+
+    it('should write contacts', function () {
+
+    });
+
+});
